063c2ade886491a81993a14c5fec79529009815f
galdemglucose <88189382+galdemglucose@users.noreply.github.com>
2021-09-06 21:17:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from './components/Board';
+import Square from './components/Square';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="header">Tic Tac Toe</h1>
+    <Board />
+    </div>
+  );
+}
+
+export default App;
